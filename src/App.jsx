c747a7a36fc60fbf92e3bbee0242ed77c66e7e44
ThePianist <cafarimo@outlook.com>
2023-05-29 17:38:56 -0400

    Add background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
     <>
+      <div className="absolute inset-0 -z-20 bg-[url(/src/assets/beams.jpg)] bg-center" />
+      <div className="absolute inset-0 -z-10 bg-[url(/src/assets/grid.svg)] bg-center" />
     </>
   )
 }
