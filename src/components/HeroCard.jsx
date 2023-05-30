7b8b301bbb2e065a33e68c12a6c4896182f2dc4f
ThePianist <cafarimo@outlook.com>
2023-05-30 05:40:05 -0400

    Show hero info on cards

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,6 +1,25 @@
+import heartImage from "../assets/heart.svg";
+import notFound from "../assets/404.png";
+
 export default function HeroCard({data}) {
-  return (<div className="flex flex-col rounded-lg bg-gray-50">
-      <img src={data["image"]["url"]} className="max-h-[300px]" />
-      <h4>{data["name"]}</h4>
-    </div>);
+  function onImageError(e) {
+    e.target.onerror = null;
+    e.target.src = notFound;
+  }
+
+  return (<div className="relative flex flex-col rounded-lg overflow-hidden bg-gray-50 shadow-xl">
+    <img src={data["image"]["url"]} className="max-h-[300px] object-cover" onError={onImageError} />
+    <div className="absolute top-0 right-0">
+      <img src={heartImage} />
+      <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-4 text-white">{data["hp"]}</p>
+    </div>
+    <div className="p-4">
+      <h3>{data["name"]}</h3>
+      <div className="mt-2 flex flex-col items-start">
+        <button className="p-1 rounded-lg hover:border-2">🧠 Mental attack ({data["attacks"]["mental"]})</button>
+        <button className="p-1 rounded-lg hover:border-2">💪 Strong attack ({data["attacks"]["strong"]})</button>
+        <button className="p-1 rounded-lg hover:border-2">⚡️ Fast attack ({data["attacks"]["fast"]})</button>
+      </div>
+    </div>
+  </div>);
 }
