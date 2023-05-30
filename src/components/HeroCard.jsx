8f31dc3d9eef175c7e0c0b9a3c55c3bedaf9fb28
ThePianist <cafarimo@outlook.com>
2023-05-30 14:36:45 -0400

    Improve UX for selecting attack

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -10,7 +10,7 @@ export default function HeroCard({data, attackSelectable, targetSelectable, sele
   }
 
   function checkAttackSelectable() {
-    return (attackSelectable ? "hover:border-2 hover:cursor-pointer animate-pulse" : "");
+    return (attackSelectable ? "bg-gray-300 hover:border-2 hover:cursor-pointer animate-pulse" : "");
   }
 
   function checkTargetSelectable() {
@@ -32,7 +32,7 @@ export default function HeroCard({data, attackSelectable, targetSelectable, sele
       className={`relative flex flex-col rounded-lg overflow-hidden bg-gray-50 shadow-xl ${checkTargetSelectable()}`}
       onClick={() => targetSelectable && onTargetSelected(data)}
     >
-      <img src={data["image"]["url"]} className="max-h-[300px] object-cover" onError={onImageError}/>
+      <img src={data["image"]["url"]} className="max-h-72 object-cover" onError={onImageError}/>
       <div className="absolute top-0 right-0">
         <img src={heartImage}/>
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-4 text-white">
@@ -49,21 +49,21 @@ export default function HeroCard({data, attackSelectable, targetSelectable, sele
       </div>
       <div className="p-4">
         <h3>{data["name"]}</h3>
-        <div className="mt-2 flex flex-col items-start">
+        <div className="mt-2 flex flex-col items-start gap-0.5">
           <p
-            className={`p-1 rounded-lg ${checkAttackSelectable()} ${checkSelectedAttack("mental")}`}
+            className={`p-1 rounded-lg w-full ${checkAttackSelectable()} ${checkSelectedAttack("mental")}`}
             onClick={() => attackSelectable && onAttackSelected([data, "mental"])}
           >
             🧠 Mental atk. ({data["attacks"]["mental"]})
           </p>
           <p
-            className={`p-1 rounded-lg ${checkAttackSelectable()} ${checkSelectedAttack("strong")}`}
+            className={`p-1 rounded-lg w-full ${checkAttackSelectable()} ${checkSelectedAttack("strong")}`}
             onClick={() => attackSelectable && onAttackSelected([data, "strong"])}
           >
             💪 Strong atk. ({data["attacks"]["strong"]})
           </p>
           <p
-            className={`p-1 rounded-lg ${checkAttackSelectable()} ${checkSelectedAttack("fast")}`}
+            className={`p-1 rounded-lg w-full ${checkAttackSelectable()} ${checkSelectedAttack("fast")}`}
             onClick={() => attackSelectable && onAttackSelected([data, "fast"])}
           >
             ⚡ Fast atk. ({data["attacks"]["fast"]})
